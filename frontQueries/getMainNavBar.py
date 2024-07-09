cc86efdf85c8586321fc362db2d31cbf94d957e4
Vu Nguyen <nghaminhvu@gmail.com>
2024-07-09 08:58:47 +0700

    Update getMainNavBar.py

--- frontQueries/getMainNavBar.py
+++ frontQueries/getMainNavBar.py
@@ -13,6 +13,12 @@ parent_list = soup.find('ul', class_='parent')
 main_nav_bar_elements = parent_list.find_all('li')
 
 
+def check_if_exists(url_to_check):
+    with db_session() as session:
+        if session.query(MainNavBar.url).filter(MainNavBar.url == url_to_check).first() is None:
+            return 0
+        return 1
+
 def get_main_navbar():
     with db_session() as session:
         for element in main_nav_bar_elements:
@@ -26,12 +32,15 @@ def get_main_navbar():
             class_names = element.a.get('title', [])
             name = ' '.join(class_names) if isinstance(class_names, list) else class_names
 
-            # Create MainNavBar object
-            element_to_db = MainNavBar(
-                url=url,
-                name=name
-            )
-            session.add(element_to_db)
+            if check_if_exists(url) == 1:
+                continue
+            else:
+                # Create MainNavBar object
+                element_to_db = MainNavBar(
+                    url=url,
+                    name=name
+                )
+                session.add(element_to_db)
         session.commit()
 
 
